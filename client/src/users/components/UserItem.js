f696342f839173c639a2c55d1d009956b8ceafc2
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 03:13:12 -0700

    add UserItem component

--- client/src/users/components/UserItem.js
+++ client/src/users/components/UserItem.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../../shared/UIElements/Avatar";
+import Card from "../../shared/UIElements/Card";
 import "./UserItem.css";
 
 const UserItem = ({ id, name, image, places }) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>
-            {places} {places > 1 ? "Places" : "Place"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {places} {places > 1 ? "Places" : "Place"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
