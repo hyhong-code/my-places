f55dd929fc9d0d1cbb2542e01f80d22827af0faf
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 03:32:02 -0700

    add Navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Users} />
-      <Route exact path="/places/new" component={NewPlace} />
-      <Redirect to="/" />
-    </Switch>
+    <Fragment>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Users} />
+          <Route exact path="/places/new" component={NewPlace} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Fragment>
   );
 };
 
