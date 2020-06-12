658eee26f3738ba8fa679f29c2eea250c45294eb
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 09:03:26 -0700

    attach Sidedrawer to MainNavigation

--- client/src/shared/components/Navigation/MainNavigation.js
+++ client/src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">My Places</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">My Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 };
 
