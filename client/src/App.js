1a3229d23c557d6b76049fba3c927312ebb71e9a
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 02:35:27 -0700

    setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => {
-  return <h1>Let's start!</h1>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Users} />
+      <Route exact path="/places/new" component={NewPlace} />
+      <Redirect to="/" />
+    </Switch>
+  );
 };
 
 export default App;
