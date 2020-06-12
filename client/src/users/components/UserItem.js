6548f11fe9b011077f23a8f20acbbd0754413d76
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 02:55:50 -0700

    add UserItem component

--- client/src/users/components/UserItem.js
+++ client/src/users/components/UserItem.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./UserItem.css";
 
-const UserItem = () => {
-  return <div></div>;
+const UserItem = ({ id, name, image, places }) => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={image} alt={name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{name}</h2>
+          <h3>
+            {places} {places > 1 ? "Places" : "Place"}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default UserItem;
