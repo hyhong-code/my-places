00814c158c01b599a1d03c443aa22e9833b322b2
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 13:11:29 -0700

    bring in useReducer hook for custom Input control

--- client/src/shared/components/FormElements/Input.js
+++ client/src/shared/components/FormElements/Input.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Input.css";
 
-const Input = ({ id, label, element, type, placeholder, rows }) => {
+const inputReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "CHANGE":
+      return { ...state, value: payload, isValid: true };
+    default:
+      return state;
+  }
+};
+
+const Input = ({ id, label, element, type, placeholder, rows, errorText }) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+  });
+
+  const changeHandler = (evt) => {
+    dispatch({
+      type: "CHANGE",
+      payload: evt.target.value,
+    });
+  };
+
   const elem =
     element === "input" ? (
-      <input id={id} type={type} placeholder={placeholder} />
+      <input
+        onChange={changeHandler}
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={id} rows={rows || 3}></textarea>
+      <textarea
+        onChange={changeHandler}
+        id={id}
+        rows={rows || 3}
+        value={inputState.value}
+      />
     );
 
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && "form-control--invalid"
+      }`}
+    >
       <label htmlFor={id}>{label}</label>
       {elem}
+      {!inputState.isValid && <p>{errorText}</p>}
     </div>
   );
 };
